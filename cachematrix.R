a27bc1ada73a9ea224136f9e328dfee66bf00aa7
Toshiaki Komura <toshiakiko04@gmail.com>
2021-06-19 22:25:04 +0900

    Assignment2

---
+++ cachematrix.R
@@ -0,0 +1,28 @@
+#This makes a matrix
+
+makeCacheMatrix<-function(x=matrix()){
+  a<-NULL
+  set<-function(y){
+    x<<-y
+    a<<-NULL
+  }
+  get<-function()x
+  setinverse<-function(inverse) a<<-inverse
+  getinverse<-function() a
+  list(set=set,setinverse=setinverse,getinverse=getinverse)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve<-function(x,...){
+  b<-x$getinverse()
+  if(!is.null(b)){
+    message("getting cached data")
+    return(b)
+  }
+  data<-x$get()
+  b<-solve(data,...)
+  x$setinverse(b)
+  b
+}
